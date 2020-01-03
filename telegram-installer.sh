994b30901b1aa9914eaf3f4c54b35cf0d5ac43b5
P1ratRuleZZZ <p1ratrulezzz@users.noreply.github.com>
2020-01-03 18:10:21 +0300

    Add files via upload

---
+++ telegram-installer.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+echo "============================================="
+echo "==     Telegram Script Installer v 0.1     =="
+echo "==                                         =="
+echo "==            by Jalcaldea                 =="
+echo "============================================="
+
+echo "Downloading necesary files..."
+
+cd /tmp
+
+LINK64="https://github.com/telegramdesktop/tdesktop/releases/download/v1.9.3/tsetup.1.9.3.tar.xz"
+LINK32="https://github.com/telegramdesktop/tdesktop/releases/download/v1.9.3/tsetup32.1.9.3.tar.xz"
+
+LINK=$LINK32
+
+if [ $(uname -p) == "x86_64" ]; then
+  LINK=$LINK64
+fi
+
+wget -O - $LINK > tsetup.tar.gz
+wget -O - https://raw.githubusercontent.com/telegramdesktop/tdesktop/master/Telegram/Telegram/Images.xcassets/Icon.iconset/icon_256x256@2x.png > icon.png
+
+echo "Making destination folder..."
+
+sudo mkdir /usr/share/telegram
+sudo chmod +x /usr/share/telegram
+
+echo "Extracting files..."
+
+tar -xvJf tsetup.tar.gz
+cd ./Telegram
+
+echo "Copying new files..."
+sudo cp ./Updater /usr/share/telegram/Updater
+sudo cp ./Telegram /usr/share/telegram/Telegram
+
+
+echo "Making desktop files..."
+
+cd /tmp
+
+sudo echo "[Desktop Entry]" > telegram.desktop
+sudo echo "Name=Telegram" >> telegram.desktop
+sudo echo "GenericName=Chat" >> telegram.desktop
+sudo echo "Comment=Chat with yours friends" >> telegram.desktop
+sudo echo "Exec=/usr/share/telegram/Telegram" >> telegram.desktop
+sudo echo "Terminal=false" >> telegram.desktop
+sudo echo "Type=Application" >> telegram.desktop
+sudo echo "Icon=/usr/share/telegram/icon.png" >> telegram.desktop
+sudo echo "Categories=Network;Chat;" >> telegram.desktop
+sudo echo "StartupNotify=false" >> telegram.desktop
+
+sudo cp icon.png /usr/share/telegram/icon.png
+sudo cp telegram.desktop /usr/share/applications/telegram.desktop
+
+echo "Removing old files..."
+
+rm /tmp/tsetup.tar.gz
+rm /tmp/icon.png
+rm /tmp/telegram.desktop
+rm -R /tmp/Telegram
+
+
+echo "Installation Complete! Launching Telegram..."
+
+/usr/share/telegram/Updater &
