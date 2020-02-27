b40e62c4b488439d2400a90feda22e6053e384f9
P1ratRuleZZZ <p1ratrulezzz@users.noreply.github.com>
2020-02-27 12:34:42 +0300

    Update telegram-installer.sh

--- telegram-installer.sh
+++ telegram-installer.sh
@@ -24,8 +24,8 @@ wget -O - https://raw.githubusercontent.com/telegramdesktop/tdesktop/master/Tele
 
 echo "Making destination folder..."
 
-sudo mkdir /usr/share/telegram
-sudo chmod +x /usr/share/telegram
+sudo mkdir /opt/telegram
+sudo chmod +x /opt/telegram
 
 echo "Extracting files..."
 
@@ -33,8 +33,10 @@ tar -xvJf tsetup.tar.gz
 cd ./Telegram
 
 echo "Copying new files..."
-sudo cp ./Updater /usr/share/telegram/Updater
-sudo cp ./Telegram /usr/share/telegram/Telegram
+sudo cp ./Updater /opt/telegram/
+sudo cp ./Telegram /opt/telegram/
+
+sudo chmod -R +x /opt/telegram/
 
 
 echo "Making desktop files..."
@@ -45,14 +47,14 @@ sudo echo "[Desktop Entry]" > telegram.desktop
 sudo echo "Name=Telegram Desktop" >> telegram.desktop
 sudo echo "GenericName=Chat" >> telegram.desktop
 sudo echo "Comment=Official desktop application for the Telegram messaging service" >> telegram.desktop
-sudo echo "Exec=/usr/share/telegram/Telegram -- %u" >> telegram.desktop
+sudo echo "Exec=/opt/telegram/Updater -- %u" >> telegram.desktop
 sudo echo "Terminal=false" >> telegram.desktop
 sudo echo "Type=Application" >> telegram.desktop
-sudo echo "Icon=/usr/share/telegram/icon.png" >> telegram.desktop
+sudo echo "Icon=/opt/telegram/icon.png" >> telegram.desktop
 sudo echo "Categories=Network;Chat;" >> telegram.desktop
 sudo echo "StartupNotify=false" >> telegram.desktop
 
-sudo cp icon.png /usr/share/telegram/icon.png
+sudo cp icon.png /opt/telegram/icon.png
 sudo cp telegram.desktop /usr/share/applications/telegram.desktop
 
 echo "Removing old files..."
@@ -65,4 +67,4 @@ rm -R /tmp/Telegram
 
 echo "Installation Complete! Launching Telegram..."
 
-/usr/share/telegram/Updater &
+/opt/telegram/Updater &
